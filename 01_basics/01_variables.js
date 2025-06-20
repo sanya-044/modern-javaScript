32e46811d34950fcab489a5222d76dcaa4c2003c
sanya-044 <sanyachauhan453@gmail.com>
2025-06-20 22:31:21 +0000

    dataTypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId= 144553  //constant keyword . it locks the value 
+let accountEmai="sanyachauhan453@gmail.com"
+let accountPassword="123"
+accountCity="jaipur"
+let accountState;
+// accountId=2 not allowed to change
+accountEmail="hch"
+accountPassword="654"
+accountCity="bengaluru"
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword, accountCity, acccountState])
+/*
+prefer not to use var 
+becuase of issue in block scope and functional scope
+
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict "; //treat all js code a newer version
+//alert(3+3) // we are using nodejs not browser
+console.log(3+
+    3
+);// code readability should be high
+
+let name="sanya"
+let age=18
+let isLoggedIn=false
+
+//primitivi datatypes
+//number => 2 to the power 53
+//bigint
+//string=>" "
+// boolean=yes/no
+//null =>its a standalone value in js , null can be assigned. its is empty value
+//undefined =>variable declared but not assigned value 
+// symbol => unique
+
+
+//object 
+
+console.log(typeof "sanya")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
