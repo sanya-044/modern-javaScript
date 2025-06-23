eb2f62d83f491fa8afc89f862e1d0c2a8102ee01
sanya-044 <sanyachauhan453@gmail.com>
2025-06-23 22:34:11 +0000

    NUMBERS

--- 01_basics/06_numbers_maths.js
+++ 01_basics/06_numbers_maths.js
@@ -1,4 +1,18 @@
-// c
+const score =400
+console.log(score)
+
+const balance =new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const othernumber=23.8966
+console.log(othernumber.toPrecision(3))
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
 //// MATHS //////
 
 console.log(Math)
