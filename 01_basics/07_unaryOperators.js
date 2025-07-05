1f9df7bcf203d78dee480005dcb3b6a3f4bdefad
sanya-044 <sanyachauhan453@gmail.com>
2025-07-05 20:58:10 +0000

    oper

---
+++ 01_basics/07_unaryOperators.js
@@ -0,0 +1,12 @@
+//pre increment (change, then use)
+let age=10;
+let newAge=++age; // first age val will be updated to 11 and then its assigned to newAge
+console.log(newAge)
+console.log(age)
+
+
+//post increment (use,then change)
+let agee=10
+let newAgee=agee++  //first agee's old val will be stored to newAgee thats 10 and then agee val will be updated to 11 
+console.log(agee)
+console.log(newAgee)
