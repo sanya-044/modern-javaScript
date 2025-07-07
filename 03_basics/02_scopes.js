8431f80dd6f4305aaab26324292bd21830aec183
sanya-044 <sanyachauhan453@gmail.com>
2025-07-07 23:08:16 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,12 @@
+var c= 300
+if(true){
+let a =10
+const b=20
+var c =30
+}
+
+
+
+// console.log(a);
+// console.log(b)
+console.log(c) //here 30 will be printed but it shouldnt print it bcz it is outside the scope . thats the problem with var
