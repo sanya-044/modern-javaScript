036cbe16c78d1fac4d9cb334bc05732d91d2640e
sanya-044 <sanyachauhan453@gmail.com>
2025-07-07 08:10:40 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+//ways to declare array
+const myArr=[1,7,8,6,9] 
+const myHeroes=['mom','dad','brother']
+
+const myArr2=new Array(2,4,6,7)
+console.log(myArr[0])
+
+
+//array methods 
+myArr.push(7) //adds element
+console.log(myArr)
+myArr.pop() //removes last element 
+ myArr.unshift(9 ) //9 will be shifted at start of the array
