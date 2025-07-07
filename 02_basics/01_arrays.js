4ad4671f8e058e1bd26fa101d1c3d8f041c5fdac
sanya-044 <sanyachauhan453@gmail.com>
2025-07-07 08:25:10 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -10,4 +10,19 @@ console.log(myArr[0])
 myArr.push(7) //adds element
 console.log(myArr)
 myArr.pop() //removes last element 
- myArr.unshift(9 ) //9 will be shifted at start of the array
+ myArr.unshift(9) //9 will be shifted at start of the array
+ myArr.shift() //removes first element of array
+
+ console.log(myArr.includes(0)) //checks whether 0 is present in arr or not and returns boolean value accordingly
+console.log(myArr.indexOf(77)) //checks the index of the provided element
+//
+const newArr=myArr.join()  //changes the array to string type
+console.log(myArr)
+console.log(newArr) //changed the type of newArr to string
+console.log(typeof newArr) //returns string
+
+//slice , splice method
+console.log('A',myArr)
+const myn1=myArr.slice(1,3)
+console.log(myn1)
+console.log('B', myArr)
