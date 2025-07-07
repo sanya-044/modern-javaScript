010c1c331bcad4a62dab964898250c647189c185
sanya-044 <sanyachauhan453@gmail.com>
2025-07-07 08:36:17 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -24,5 +24,11 @@ console.log(typeof newArr) //returns string
 //slice , splice method
 console.log('A',myArr)
 const myn1=myArr.slice(1,3)
+
 console.log(myn1)
 console.log('B', myArr)
+
+const myn2=myArr.splice(1,3)
+console.log('C', myArr)
+console.log(myn2)
+
