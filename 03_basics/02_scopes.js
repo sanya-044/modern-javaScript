eab5c8cf2c9e6c8023c1555d7bb64ff6d017fb17
sanya-044 <sanyachauhan453@gmail.com>
2025-07-08 20:46:13 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,3 +10,31 @@ var c =30
 // console.log(a);
 // console.log(b)
 console.log(c) //here 30 will be printed but it shouldnt print it bcz it is outside the scope . thats the problem with var
+
+//nested scope
+
+function one(){
+    const username="hitesh"
+
+    function two(){
+        const website ='youtube'
+        console.log(username)
+    }
+   // console.log(website) // throws error becoz we cant access function one through function 2 but vice vera happens bcoz for function 2 ..func1 is global
+    two()
+
+}
+one()
+
+// intresting//
+console.log(addone(3))    //runs 
+function addone(num){
+return num+1
+}
+
+//
+
+console.log(addTwo(9))    //throws error
+const addTwo=function(num){
+    return num+2
+}
