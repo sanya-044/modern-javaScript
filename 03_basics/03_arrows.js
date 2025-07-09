e251790e3f269ac465f052638c3e184462060199
sanya-044 <sanyachauhan453@gmail.com>
2025-07-09 09:11:04 +0000

    arrows

---
+++ 03_basics/03_arrows.js
@@ -0,0 +1,14 @@
+const user={
+    username:'sanya',
+    price:99,
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to website`)
+        
+    }
+}
+//console.log(typeof user.welcomeMessage())
+ user.welcomeMessage()
+//  user.username='sam'
+//  user.welcomeMessage()
+ //console.log(this)
