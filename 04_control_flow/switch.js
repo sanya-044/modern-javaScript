20dcd71b7c39ec5a7fc8b2936bc0d3e1e6f927e5
Sanya Chauhan <sanyachauhan453@gmail.com>
2025-07-12 08:29:12 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch(key){
+//     case value:
+                                   //sytntax of switch case
+//     break;
+
+// default:                       
+//      break;
+// }
+
+const month=3;
+switch (month) {
+    case 1:
+        console.log('jan')
+        break;
+    case 2:
+        console.log('feb')
+        break;
+    case 3:
+        console.log('march')
+        break; //without break april would also be executed  
+    case 4:
+        console.log('april')
+        break;
+     
+         
+ 
+    default:
+        console.log('default executes if nothing executes above')
+        break;
+}
