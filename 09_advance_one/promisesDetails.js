09210c319191ad0b779c7051eabe3b6a86c0bdf1
sanya-044 <sanyachauhan453@gmail.com>
2025-07-19 10:14:31 -0700

    promises

--- 09_advance_one/promisesDetails.js
+++ 09_advance_one/promisesDetails.js
@@ -11,8 +11,7 @@
   }
   setTimeout(hello,2000) //it says that hello function will execute after 2 seconds
 
-
-//another way to write setTimeout
+  //another way to write setTimeout
 setTimeout(()=>{
     console.log('hello')
 },4000)
@@ -25,3 +24,39 @@ setTimeout(()=>{
     console.log('hello')
 },4000)
 console.log('three')      //here three wont wait for the setTimeout to print hello . one , two, three will execute simultenously and 4 seconds later hello will be printed
+
+
+
+//CALLBACKS= a callback is a function passed as an argument to another function
+function sum(a,b){
+  console.log(a+b);
+}
+function calculator(a,b, sumCallback){
+  sumCallback(a,b)
+}
+calculator(1,2,sum) 
+
+//or
+  function sum(a,b){
+   console.log(a+b);
+}
+function calculator(a,b, sumCallback){
+  sumCallback(a,b)
+}
+calculator(1,2,(a,b)=>{
+  console.log(a+b)
+}) 
+
+
+//callback hell= nested callbacks stacked below one another forming a pyramid structure(pyramid of doom). this style of programming becomes difficult to understand and manage 
+
+
+ 
+
+//
+function getdata(dataid){   
+  console.log('data', dataid)
+}
+
+
+
